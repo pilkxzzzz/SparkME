3351dfb83526bc7bc89bbf0a68e32ae4f53f857e
pilk xz <pilkxzz@gmail.com>
2025-01-20 02:44:10 +0000

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// Ініціалізуємо Supabase
-const supabaseUrl = 'https://hqqqneqjqvjaemjusxub.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxcXFuZXFqcXZqYWVtanVzeHViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzczMTMzNjQsImV4cCI6MjA1Mjg4OTM2NH0.1uV4_zyh1AouKmEMSqgvUDpQG4RoF_ppSZDel79wsIk';
-
-export const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
